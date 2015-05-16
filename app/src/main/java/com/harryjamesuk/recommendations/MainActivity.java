8d2adb234b8e42ea2ce0c4e555f5b278f9d08e1b
Harry James <harryjamesuk@users.noreply.github.com>
2015-05-16 11:53:09 +0100

    UI Preparations
    Added our RecyclerView, ProgressBar and TextView for an error. Put all
    of these in a FrameLayout (Draws child views in a stack).
    
    In MainActivity, added references to the views and created 3 new
    methods:
    showLoading() - Shows the progressBar only.
    showList() - Shows the list only.
    showError() - shows the error only.
    
    showLoading() is called when the app first runs.

--- app/src/main/java/com/harryjamesuk/recommendations/MainActivity.java
+++ app/src/main/java/com/harryjamesuk/recommendations/MainActivity.java
@@ -2,16 +2,29 @@ package com.harryjamesuk.recommendations;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private RecyclerView recyclerView;
+    private View progressBar;
+    private TextView errorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        progressBar = findViewById(R.id.progressBar);
+        errorView = (TextView) findViewById(R.id.errorView);
+
+        showLoading();
     }
 
     @Override
@@ -34,5 +47,23 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showLoading() {
+        progressBar.setVisibility(View.VISIBLE);
+        recyclerView.setVisibility(View.GONE);
+        errorView.setVisibility(View.GONE);
+    }
+
+    public void showList() {
+        progressBar.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.VISIBLE);
+        errorView.setVisibility(View.GONE);
+    }
+
+    public void showError() {
+        progressBar.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.GONE);
+        errorView.setVisibility(View.VISIBLE);
     }
 }
