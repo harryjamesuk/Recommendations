db4a073448b0b28321037abbcbf89b13851d9539
Harry James <harryjamesuk@users.noreply.github.com>
2015-05-16 11:41:23 +0100

    Created ListingAdapter
    After we get our data from the Etsy API using Retrofit, we'll then be
    brought to this method. Here, we'll set the activeListings through the
    success() method and notifyDataSetChanged().
    
    Nested a ListingHolder class that will be the ViewHolder. This will just
    store all of the views together. This just saves us having too many
    findViewById() calls that could prove costly in terms of performance.
    
    In ListingAdapter's constructor, we'll get the context and our
    LayoutInflater from the context.
    In onCreateViewHolder(), we'll create a ListingHolder and inflate our
    views using the inflater.
    In onBindViewHolder(), we'll get the current listing and set everything.
    In getItemCount(), check we don't have null and return the length.

---
+++ app/src/main/java/com/harryjamesuk/recommendations/ListingAdapter.java
@@ -0,0 +1,85 @@
+package com.harryjamesuk.recommendations;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.harryjamesuk.recommendations.model.ActiveListings;
+import com.harryjamesuk.recommendations.model.Listing;
+import com.squareup.picasso.Picasso;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+/**
+ * Created by Harry on 15/05/2015.
+ */
+public class ListingAdapter extends RecyclerView.Adapter<ListingAdapter.ListingHolder> implements Callback<ActiveListings> {
+
+    public LayoutInflater inflater;
+    private ActiveListings activeListings;
+
+    public ListingAdapter(Context context) {
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public ListingHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        return new ListingHolder(inflater.inflate(R.layout.layout_listing, viewGroup, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ListingHolder listingHolder, int i) {
+        final Listing listing = activeListings.results[i];
+        listingHolder.titleView.setText(listing.title);
+        listingHolder.priceView.setText(listing.price);
+        listingHolder.shopNameView.setText(listing.Shop.shop_name);
+
+        Picasso.with(listingHolder.imageView.getContext())
+                .load(listing.Images[0].url_570xN)
+                .into(listingHolder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (activeListings == null)
+            return 0;
+
+        if (activeListings.results == null)
+            return 0;
+
+        return activeListings.results.length;
+    }
+
+    @Override
+    public void success(ActiveListings activeListings, Response response) {
+        this.activeListings = activeListings;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public void failure(RetrofitError error) {
+
+    }
+
+    public class ListingHolder extends RecyclerView.ViewHolder {
+
+        public ImageView imageView;
+        public TextView titleView;
+        public TextView shopNameView;
+        public TextView priceView;
+
+        public ListingHolder(View itemView) {
+            super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.listing_image);
+            titleView = (TextView) itemView.findViewById(R.id.listing_title);
+            shopNameView = (TextView) itemView.findViewById(R.id.listing_shop_name);
+            priceView = (TextView) itemView.findViewById(R.id.listing_price);
+        }
+    }
+}
