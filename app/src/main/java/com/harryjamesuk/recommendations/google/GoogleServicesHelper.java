8847ee297116b3fd657f8954f920741fd542bd74
Harry James <harryjamesuk@users.noreply.github.com>
2015-05-16 14:56:10 +0100

    Handling states in GoogleApiClient
    Added request codes at top of class.
    
    Added two new methods, connect() and disconnect() that will run the
    appropriate methods off of the apiClient.
    
    Added isGooglePlayServicesAvailable() method that will check if we can
    use Google Play Services or if there's a problem.
    If there is a problem, we'll forward that over to the
    GooglePlayServicesUtil that will try to resolve the issue. We'll still
    return false just in case the problem was not fixed, however.

--- app/src/main/java/com/harryjamesuk/recommendations/google/GoogleServicesHelper.java
+++ app/src/main/java/com/harryjamesuk/recommendations/google/GoogleServicesHelper.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
@@ -12,6 +13,9 @@ import com.google.android.gms.common.api.GoogleApiClient;
 public class GoogleServicesHelper implements
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
+
+    private static final int REQUEST_CODE_RESOLUTION = -100;
+    private static final int REQUEST_CODE_AVAILABILITY = -101;
 
     public interface GoogleServicesListener {
         public void onConnected();
@@ -30,6 +34,30 @@ public class GoogleServicesHelper implements
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
+    }
+
+    public void connect() {
+        apiClient.connect();
+    }
+
+    public void disconnect() {
+        apiClient.disconnect();
+    }
+
+    private boolean isGooglePlayServicesAvailable() {
+        int availability = GooglePlayServicesUtil.isGooglePlayServicesAvailable(activity);
+        switch (availability) {
+            case ConnectionResult.SUCCESS:
+                return true;
+            case ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED:
+            case ConnectionResult.SERVICE_DISABLED:
+            case ConnectionResult.SERVICE_INVALID:
+                GooglePlayServicesUtil.getErrorDialog(availability, activity, REQUEST_CODE_AVAILABILITY)
+                        .show();
+                return false;
+            default:
+                return false;
+        }
     }
 
     @Override
